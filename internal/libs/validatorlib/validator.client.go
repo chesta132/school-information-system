c49f682734d676c0ece0c545847d5885f19ffd81
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-12-23 12:29:51 +0700

    feat: add register tag name for validator client

--- internal/libs/validatorlib/validator.client.go
+++ internal/libs/validatorlib/validator.client.go
@@ -1,5 +1,20 @@
 package validatorlib
 
-import "github.com/go-playground/validator/v10"
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var Client = validator.New(validator.WithRequiredStructEnabled())
+
+func RegisterTagName() {
+	Client.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+}
