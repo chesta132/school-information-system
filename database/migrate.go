657fad0f83bea27b1c217e117f4d893d946ca43d
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-12-23 12:31:46 +0700

    fix: add missing models to AutoMigrate

--- database/migrate.go
+++ database/migrate.go
@@ -2,15 +2,12 @@ package database
 
 import (
 	"log"
+	"school-information-system/internal/models"
 
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) {
-	if err := db.AutoMigrate(); err != nil {
-		log.Fatal("school-app: failed to migrate databases model", err.Error())
-	}
-
 	if err := CreateEnum(db, "user_role", []string{"student", "teacher", "admin", "unsetted"}); err != nil {
 		log.Fatal("school-app: failed to create user role enum", err.Error())
 	}
@@ -21,5 +18,18 @@ func Migrate(db *gorm.DB) {
 
 	if err := CreateEnum(db, "permission_action", []string{"create", "read", "update", "delete"}); err != nil {
 		log.Fatal("school-app: failed to create permission action enum", err.Error())
+	}
+
+	if err := db.AutoMigrate(
+		&models.User{},
+		&models.Student{},
+		&models.Teacher{},
+		&models.Admin{},
+		&models.Teacher{},
+		&models.Permission{},
+		&models.Class{},
+		&models.Subject{},
+	); err != nil {
+		log.Fatal("school-app: failed to migrate databases model", err.Error())
 	}
 }
