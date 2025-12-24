34c1e83e7489f50fb421898df8bfeb657fde69f8
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-12-24 16:57:00 +0700

    feat: add validate struct and return reply error payload helper

--- internal/libs/validatorlib/validator.lib.go
+++ internal/libs/validatorlib/validator.lib.go
@@ -3,9 +3,12 @@ package validatorlib
 import (
 	"errors"
 	"fmt"
+	"school-information-system/internal/libs/replylib"
+	"school-information-system/internal/libs/slicelib"
 	"slices"
 	"strings"
 
+	"github.com/chesta132/goreply/reply"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -38,4 +41,19 @@ func TranslateError(err error) (error, validator.ValidationErrors) {
 	}
 
 	return errors.New(strings.Join(messages, ", ")), valErrs
+}
+
+func ValidateStructToReply(s any) *reply.ErrorPayload {
+	if err := Client.Struct(s); err != nil {
+		err, valErrs := TranslateError(err)
+		fields := slicelib.Unique(slicelib.Map(valErrs, func(i int, val validator.FieldError) string { return val.Field() }))
+
+		return &reply.ErrorPayload{
+			Code:    replylib.CodeBadRequest,
+			Message: "invalid payload",
+			Details: err.Error(),
+			Fields:  fields,
+		}
+	}
+	return nil
 }
