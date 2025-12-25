71ce3ddd40054ac57fccc05d36093889279bd014
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-12-25 11:51:34 +0700

    feat: add set role route

--- internal/handlers/admin.handler.go
+++ internal/handlers/admin.handler.go
@@ -12,11 +12,12 @@ import (
 )
 
 type Admin struct {
-	adminService *services.Admin
+	adminService      *services.Admin
+	roleSetterService *services.RoleSetter
 }
 
-func NewAdmin(adminService *services.Admin) *Admin {
-	return &Admin{adminService}
+func NewAdmin(adminService *services.Admin, roleSetterService *services.RoleSetter) *Admin {
+	return &Admin{adminService, roleSetterService}
 }
 
 func (h *Admin) InitiateAdmin(c *gin.Context) {
@@ -35,3 +36,19 @@ func (h *Admin) InitiateAdmin(c *gin.Context) {
 	rp.Success(user).Info(fmt.Sprintf("Admin created: %s (%s)", user.FullName, payload.StaffRole)).OkJSON()
 }
 
+func (h *Admin) SetRole(c *gin.Context) {
+	rp := replylib.Client.New(adapter.AdaptGin(c))
+	var payload payloads.RequestSetRole
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		rp.Error(replylib.CodeBadRequest, err.Error()).FailJSON()
+		return
+	}
+
+	user, errPayload := h.roleSetterService.ApplyContext(c).SetRole(payload)
+	if errPayload != nil {
+		rp.Error(errPayload.Code, errPayload.Message, reply.OptErrorPayload{Details: errPayload.Details, Fields: errPayload.Fields}).FailJSON()
+		return
+	}
+
+	rp.Success(user).Info(fmt.Sprintf("%s's role setted", user.FullName)).OkJSON()
+}
--- internal/routes/admin.route.go
+++ internal/routes/admin.route.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"school-information-system/internal/handlers"
 	"school-information-system/internal/middlewares"
+	"school-information-system/internal/models"
 	"school-information-system/internal/repos"
 	"school-information-system/internal/services"
 
@@ -13,11 +14,23 @@ func (rt *Route) RegisterAdmin(group *gin.RouterGroup) {
 	userRepo := repos.NewUser(rt.db)
 	adminRepo := repos.NewAdmin(rt.db)
 	revokedRepo := repos.NewRevoked(rt.db)
+	// role setter repos
+	studentRepo := repos.NewStudent(rt.db)
+	classRepo := repos.NewClass(rt.db)
+	parentRepo := repos.NewParent(rt.db)
+	teacherRepo := repos.NewTeacher(rt.db)
+	subjectRepo := repos.NewSubject(rt.db)
 
 	adminService := services.NewAdmin(userRepo, adminRepo)
+	roleSetterService := services.NewRoleSetter(userRepo, adminRepo, studentRepo, classRepo, parentRepo, teacherRepo, subjectRepo)
 
-	handler := handlers.NewAdmin(adminService)
+	handler := handlers.NewAdmin(adminService, roleSetterService)
 	mw := middlewares.NewAuth(userRepo, revokedRepo)
 
 	group.POST("/initiate", mw.Protected(true), handler.InitiateAdmin)
+	group.POST(
+		"/set-role",
+		mw.PermissionProtected(models.ResourceRole, []models.PermissionAction{models.ActionCreate, models.ActionUpdate, models.ActionRead}),
+		handler.SetRole,
+	)
 }
