cfcdd42e4ac441a52c9ce56deb073dea10f1edd5
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-12-29 10:06:30 +0700

    fix: enum not updated

--- database/utils.go
+++ database/utils.go
@@ -13,15 +13,38 @@ func CreateEnum(db *gorm.DB, name string, enums []string) error {
 	}
 	enum := strings.Join(enums, ",")
 
-	query := fmt.Sprintf(`
-		DO $$
-		BEGIN
-				IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = '%s') THEN
-						CREATE TYPE %s AS ENUM (%s);
-				END IF;
-		END
-		$$;
-	`, name, name, enum)
+	// check existing value of enum from name
+	var existingValues []string
+	err := db.Raw(`
+		SELECT e.enumlabel 
+		FROM pg_type t 
+		JOIN pg_enum e ON t.oid = e.enumtypid 
+		WHERE t.typname = ?
+		ORDER BY e.enumsortorder
+	`, name).Scan(&existingValues).Error
 
+	if err == nil && len(existingValues) > 0 {
+		if len(existingValues) == len(enums) {
+			allSame := true
+			for i, val := range existingValues {
+				if "'"+val+"'" != enums[i] {
+					allSame = false
+					break
+				}
+			}
+			if allSame {
+				// skip same
+				return nil
+			}
+		}
+
+		// delete to update
+		if err := db.Exec(fmt.Sprintf("DROP TYPE IF EXISTS %s CASCADE", name)).Error; err != nil {
+			return err
+		}
+	}
+
+	// create enum
+	query := fmt.Sprintf("CREATE TYPE %s AS ENUM (%s)", name, enum)
 	return db.Exec(query).Error
 }
