291b29ad5bc708db560c24f5136ce46f5844bd86
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-12-30 13:37:29 +0700

    feat: update class model

--- internal/models/class.model.go
+++ internal/models/class.model.go
@@ -1,10 +1,20 @@
 package models
 
+import "fmt"
+
 type Class struct {
 	Id
-	Name          string   `json:"name" example:"10 TJKT 3"`
+	Grade         int      `json:"grade" example:"10"`
+	Major         string   `json:"major" example:"TJKT"`
+	ClassNumber   int      `json:"class_number" example:"3"`
+	Name          string   `json:"name" gorm:"-" example:"10 TJKT 3"`
 	FormTeacherID string   `json:"-"`
 	FormTeacher   *Teacher `json:"form_teacher,omitempty" swaggerignore:"true"`
 
 	Timestamp
+}
+
+func (c *Class) GetName() string {
+	c.Name = fmt.Sprintf("%d %s %d", c.Grade, c.Major, c.ClassNumber)
+	return c.Name
 }
